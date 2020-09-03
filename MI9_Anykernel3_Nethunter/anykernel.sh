c23f95aa27e2c67c4d6970f87560c9c3862ba9b8
homeauto <lilldeck@gmail.com>
2020-09-03 12:29:35 +0200

    Update anykernel.sh
    add nethunter

--- MI9_Anykernel3_Nethunter/anykernel.sh
+++ MI9_Anykernel3_Nethunter/anykernel.sh
@@ -23,7 +23,12 @@ ramdisk_compression=auto;
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
-
+	
+## NetHunter additions
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 ## AnyKernel install
 dump_boot;
@@ -125,6 +130,10 @@ fi;
 # patch_cmdline "scsi_mod.use_blk_mq=0";
 # ui_print "  • Enabling MQ-Schedulers"
 
+backup_file init.rc;
+insert_line init.rc "init.nethunter.rc" after "import /init.usb.configfs.rc" "import /init.nethunter.rc";
+backup_file ueventd.rc;
+insert_line ueventd.rc "/dev/hidg" after "/dev/pmsg0" "/dev/hidg*                0666   root       root";
 
 # end ramdisk changes
 
