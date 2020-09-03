1241240e9c7b95920ec85c70f25bf769b88a541b
homeauto <lilldeck@gmail.com>
2020-09-03 11:50:39 +0200

    Update setup.sh
    some fixes to build stock kernel

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ git clone https://github.com/homeauto/nethunter.git
 
 git clone https://github.com/homeauto/toolchains.git
 
-git clone https://github.com/homeauto/QK-AOSP-Cepheus -b nethunter-10.0
+git clone https://github.com/Official-Ayrton990/Quantic-Kernel-AOSP-Cepheus.git -b PELT
 
-cp ~/nethunter/build.sh ~/QK-AOSP-Cepheus
+cp ~/nethunter/build.sh ~/Quantic-Kernel-AOSP-Cepheus
 
 cd ~/
-. ./QK-AOSP-Cepheus/build.sh
+. ./Quantic-Kernel-AOSP-Cepheus/build.sh
